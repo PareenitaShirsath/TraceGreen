08a8e05ffd40321a08824d223e99bdf60281f379
Pareenita Atul Shirsath <paree.nita97@gmail.com>
2025-04-24 23:31:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -134,11 +134,12 @@ function App() {
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
-              <p>&copy; 2025 CFC - Pareenita Shirsath</p>
+              <p>&copy; 2025 TraceGreen By Pareenita, Ishita & Amruta</p>
             </div>
             <div className="flex space-x-4">
-
-               <a href="https://yashashokshirsath.netlify.app/#contact" className="hover:text-green-400" target="_blank" rel="noopener noreferrer">Contact Us</a>
+              <a href="#" className="hover:text-green-400"></a>
+              <a href="#" className="hover:text-green-400"></a>
+              <a href="#" className="hover:text-green-400"></a>
             </div>
           </div>
         </div>
